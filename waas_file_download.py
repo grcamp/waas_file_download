8ec0958453e9cd2a0ebbd6889059981153dbee01
grcamp <Gregory.Camp@gmail.com>
2017-05-05 17:02:19 -0400

    Completed download_image script

--- waas_file_download.py
+++ waas_file_download.py
@@ -18,11 +18,10 @@
 #    logger = Used for Debug output and script info
 # #########################################################################
 
-import re
-import os
 import logging
-import copy
+import time
 import argparse
+import paramiko
 import sys
 import xlsxwriter
 import json
@@ -46,197 +45,6 @@ def fatal(msg):
     logger.fatal(msg)
     exit(1)
 
-# Method wait_for_prompt
-#
-# Input: None
-# Output: None
-# Parameters: None
-#
-# Return Value: -1 on error, 0 for successful discovery
-#####################################################################
-def wait_for_prompt(self, remote_conn, myLogFile, prompt = ">", timeout=10):
-    # Declare variables
-    myOutput = ""
-    allOutput = ""
-    i = 0
-    
-    # Wait timeout seconds total
-    while i < timeout:
-        time.sleep(1)
-        myOutput = remote_conn.recv(65535)
-        allOutput = allOutput + myOutput
-        
-        if prompt in myOutput:
-            i = timeout
-
-        myLogFile.write(myOutput)
-        myLogFile.flush()
-        i = i + 1
-
-    # Return None
-    return allOutput
-
-# Method upload_config
-#
-# Input: None
-# Output: None
-# Parameters: None
-#
-# Return Value: -1 on error, 0 for successful discovery
-#####################################################################
-def upload_config(self, ipAddress, username, password, ftpUploadUser, ftpUploadPass, ftpUploadIP, ftpUploadPath, ftpUploadFile):
-    # Declare variables
-    returnVal = 0
-    downloadAttempts = 0
-
-    # Open Log File
-    myLogFile = open(ipAddress + "_log.txt",'a')
-
-    # Attempt to login to devices via SSH
-    try:
-        # Attempt Login
-        remote_conn_pre = paramiko.SSHClient()
-        # Bypass SSH Key accept policy
-        remote_conn_pre.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        # Attempt to connection
-        remote_conn_pre.connect(ipAddress,username=username,password=password,look_for_keys=False,allow_agent=False)
-        # Log into WLC
-        remote_conn = remote_conn_pre.invoke_shell()
-        time.sleep(15)
-        myOutput = remote_conn.recv(65535)
-        myLogFile.write(myOutput)
-        myLogFile.flush()
-        
-        # Check if user prompt appears
-        if "User:" not in myOutput:
-            myLogFile.close()
-            remote_conn.close()
-            return -1
-
-        remote_conn.send(username)
-        remote_conn.send("\n")
-        time.sleep(1)
-        myOutput = remote_conn.recv(65535)
-        myLogFile.write(myOutput)
-        myLogFile.flush()
-        remote_conn.send(password)
-        remote_conn.send("\n")
-        time.sleep(15)
-        myOutput = remote_conn.recv(65535)
-        myLogFile.write(myOutput)
-        myLogFile.flush()
-        
-        # Check if user prompt appears
-        if ">" not in myOutput:
-            myLogFile.close()
-            remote_conn.close()
-            return -2
-        
-        # Login successful
-        logger.info("Logged into %s" % (ipAddress))
-        
-        # Clear transfer info
-        remote_conn.send("clear transfer")
-        remote_conn.send("\n")
-        wait_for_prompt(remote_conn, myLogFile, prompt="(y/n)")
-        remote_conn.send("y")
-        remote_conn.send("\n")
-        wait_for_prompt(remote_conn, myLogFile)
-        
-        # Get current time
-        ftpUploadFile = ipAddress + ".txt"
-        
-        # Set Upload FTP parameters
-        remote_conn.send("transfer upload datatype run-config")
-        remote_conn.send("\n")
-        wait_for_prompt(remote_conn, myLogFile)
-        remote_conn.send("transfer upload mode ftp")
-        remote_conn.send("\n")
-        wait_for_prompt(remote_conn, myLogFile)
-        remote_conn.send("transfer upload serverip " + ftpUploadIP)
-        remote_conn.send("\n")
-        wait_for_prompt(remote_conn, myLogFile)
-        remote_conn.send("transfer upload path " + ftpUploadPath)
-        remote_conn.send("\n")
-        wait_for_prompt(remote_conn, myLogFile)
-        remote_conn.send("transfer upload filename " + ftpUploadFile)
-        remote_conn.send("\n")
-        wait_for_prompt(remote_conn, myLogFile)
-        remote_conn.send("transfer upload username " + ftpUploadUser)
-        remote_conn.send("\n")
-        wait_for_prompt(remote_conn, myLogFile)
-        remote_conn.send("transfer upload password " + ftpUploadPass)
-        remote_conn.send("\n")
-        wait_for_prompt(remote_conn, myLogFile)
-        # Start FTP
-        remote_conn.send("transfer upload start")
-        remote_conn.send("\n")
-        wait_for_prompt(remote_conn, myLogFile, prompt="(y/N)")
-        remote_conn.send("y")
-        remote_conn.send("\n")
-        
-        # Wait for output
-        myOutput = wait_for_prompt(remote_conn, myLogFile, timeout=60)
-
-        if "File transfer operation completed successfully." in myOutput:
-            # Set returnVal to good
-            returnVal = 0
-            # Config upload successful
-            logger.info("Run-config upload successful for %s" % (ipAddress))
-        else:
-            # Set returnVal to -3 while waiting
-            returnVal = -3
-        
-        # Clear transfer info
-        remote_conn.send("clear transfer")
-        remote_conn.send("\n")
-        wait_for_prompt(remote_conn, myLogFile, prompt="(y/n)")
-        remote_conn.send("y")
-        remote_conn.send("\n")
-        wait_for_prompt(remote_conn, myLogFile)
-        
-        # Logout
-        remote_conn.send("logout")
-        remote_conn.send("\n")
-        time.sleep(1)
-        myOutput = remote_conn.recv(65535)
-        myLogFile.write(myOutput)
-        # If asked to save config select No
-        if "(y/N)" in myOutput:
-            remote_conn.send("N")
-            remote_conn.send("\n")
-            time.sleep(2)
-            myOutput = remote_conn.recv(65535)
-            myLogFile.write(myOutput)
-            
-        # Close connection
-        remote_conn.close()
-        myLogFile.close()
-    # Print exception and return -1
-    except IOError as error:
-        print("Invalid Hostname")
-        myLogFile.close()
-        return -1
-    except paramiko.PasswordRequiredException as error:
-        print("Invalid Username or password")
-        myLogFile.close()
-        return -2
-    except paramiko.AuthenticationException as error:
-        print("Invalid Username or password")
-        myLogFile.close()
-        return -2
-    except socket.timeout as error:
-        print("Connection timeout")
-        myLogFile.close()
-        return -1
-    except Exception, e:
-        print(str(e))
-        myLogFile.close()
-        return -1
-
-    # Return success
-    return returnVal
-  
 
 #########################################################################
 # Class WAE
@@ -250,7 +58,135 @@ class WAE:
         self.password = ""
         self.ftpConfig = {}
 
-# Method write_xlsx_report
+    # Method download_image
+    #
+    # Input: None
+    # Output: None
+    # Parameters: None
+    #
+    # Return Value: -1 on error, 0 for successful discovery
+    #####################################################################
+    def download_image(self):
+        # Declare variables
+        returnVal = 0
+        downloadAttempts = 0
+
+        # Open Log File
+        myLogFile = open(self.ipAddress + "_log.txt", 'a')
+
+        # Attempt to login to devices via SSH
+        try:
+            # Attempt Login
+            remote_conn_pre = paramiko.SSHClient()
+            # Bypass SSH Key accept policy
+            remote_conn_pre.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            # Attempt to connection
+            remote_conn_pre.connect(self.ipAddress, username=self.username, password=self.password, look_for_keys=False,
+                                    allow_agent=False)
+            # Log into WLC
+            remote_conn = remote_conn_pre.invoke_shell()
+            time.sleep(15)
+            myOutput = remote_conn.recv(65535)
+            myLogFile.write(myOutput)
+            myLogFile.flush()
+
+            # Check if user prompt appears
+            if "#" not in myOutput:
+                myLogFile.close()
+                remote_conn.close()
+                return -2
+
+            # Login successful
+            logger.info("Logged into %s" % (self.ipAddress))
+
+            # Clear transfer info
+            remote_conn.send("copy ftp disk %s %s %s %s" % (self.ftpConfig['serverIp'], self.ftpConfig['filePath'],
+                                                            self.ftpConfig['fileName'], self.ftpConfig['fileName']))
+            remote_conn.send("\n")
+            wait_for_prompt(remote_conn, myLogFile, prompt="server:")
+            remote_conn.send(self.ftpConfig['username'])
+            remote_conn.send("\n")
+            wait_for_prompt(remote_conn, myLogFile, prompt="server:")
+            remote_conn.send(self.ftpConfig['password'])
+            remote_conn.send("\n")
+            wait_for_prompt(remote_conn, myLogFile)
+            wait_for_prompt(remote_conn, myLogFile, prompt="Downloaded", timeout=21600)
+            # Verify File
+            remote_conn.send("md5sum %s" % (self.ftpConfig['md5']))
+            remote_conn.send("\n")
+            myOutput = wait_for_prompt(remote_conn, myLogFile)
+
+            if self.ftpConfig['md5'] in myOutput:
+                returnVal = 0
+            else:
+                returnVal = -3
+
+            # Logout
+            remote_conn.send("exit")
+            remote_conn.send("\n")
+            time.sleep(1)
+            myOutput = remote_conn.recv(65535)
+            myLogFile.write(myOutput)
+
+            # Close connection
+            remote_conn.close()
+            myLogFile.close()
+        # Print exception and return -1
+        except IOError as error:
+            print("Invalid Hostname")
+            myLogFile.close()
+            return -1
+        except paramiko.PasswordRequiredException as error:
+            print("Invalid Username or password")
+            myLogFile.close()
+            return -2
+        except paramiko.AuthenticationException as error:
+            print("Invalid Username or password")
+            myLogFile.close()
+            return -2
+        except socket.timeout as error:
+            print("Connection timeout")
+            myLogFile.close()
+            return -1
+        except Exception, e:
+            print(str(e))
+            myLogFile.close()
+            return -1
+
+        # Return success
+        return returnVal
+
+# Method wait_for_prompt
+#
+# Input: None
+# Output: None
+# Parameters: None
+#
+# Return Value: -1 on error, 0 for successful discovery
+#####################################################################
+def wait_for_prompt(self, remote_conn, myLogFile, prompt="#", timeout=10):
+    # Declare variables
+    myOutput = ""
+    allOutput = ""
+    i = 0
+
+    # Wait timeout seconds total
+    while i < timeout:
+        time.sleep(1)
+        myOutput = remote_conn.recv(65535)
+        allOutput = allOutput + myOutput
+
+        if prompt in myOutput:
+            i = timeout
+
+        myLogFile.write(myOutput)
+        myLogFile.flush()
+        i = i + 1
+
+    # Return None
+    return allOutput
+
+# function write_xlsx_report
 #
 # Input: None
 # Output: None
@@ -299,15 +235,16 @@ def write_xlsx_report(flexGroupList, apList, outputFile):
 #
 # Return Value: None
 #####################################################################
-def build_wae_list(waasList, ftpConfig, username, password, ftpUsername, ftpPassword):
+def build_wae_list(waasList, ftpConfig, username, password):
     # Declare variables
     returnList = []
     
     logger.info("Building WAE List")
     
     # Build FTP Config
-    myFtpConfig = {'username':ftpUsername, 'password':ftpPassword, 'filePath':str(ftpConfig['filePath']), 
-    'fileName':str(ftpConfig['fileName']), 'serverIP':str(ftpConfig['serverIP']), 'md5':str(ftpConfig['md5'])}
+    myFtpConfig = {'username':str(ftpConfig['username']), 'password':str(ftpConfig['password']),
+                   'filePath':str(ftpConfig['filePath']), 'fileName':str(ftpConfig['fileName']),
+                   'serverIP':str(ftpConfig['serverIP']), 'md5':str(ftpConfig['md5'])}
     
     # Get configuration for each flex-connect group
     for line in waasList:
@@ -338,25 +275,25 @@ def download_image_worker(device):
     myDeviceNum = long(currentDevice)
     
 
-    logger.info("Starting worker for %s - %s of %s" % (str(device.ipAddress), str(myDeviceNum), str(deviceCount))
+    logger.info("Starting worker for %s - %s of %s" % (str(device.ipAddress), str(myDeviceNum), str(deviceCount)))
     i = device.download_image(myDeviceNum)
 
     # If discovered, parse data
     if i == 0:
-        logger.info("Image Download Complete for %s - %s of %s" % (str(device.ipAddress), str(myDeviceNum), str(deviceCount))
+        logger.info("Image Download Complete for %s - %s of %s" % (str(device.ipAddress), str(myDeviceNum), str(deviceCount)))
         return None
-    # Else printer error
+    # Else print error
     elif i == -2:
-        logger.info("Bad username or password for %s - %s of %s" % (str(device.ipAddress), str(myDeviceNum), str(deviceCount))
+        logger.info("Bad username or password for %s - %s of %s" % (str(device.ipAddress), str(myDeviceNum), str(deviceCount)))
     elif i == -3:
-        logger.info("Image Download Failed for %s - %s of %s" % (str(device.ipAddress), str(myDeviceNum), str(deviceCount))
+        logger.info("Image Download Failed for %s - %s of %s" % (str(device.ipAddress), str(myDeviceNum), str(deviceCount)))
     else:
-        logger.info("Image Download Failed for %s - %s of %s" % (str(device.ipAddress), str(myDeviceNum), str(deviceCount))
+        logger.info("Image Download Failed for %s - %s of %s" % (str(device.ipAddress), str(myDeviceNum), str(deviceCount)))
 
     return None
 
 
-# Method main
+# Function main
 #
 # Input: None
 # Output: None
@@ -382,8 +319,6 @@ def main(**kwargs):
         parser.add_argument('ftpConfig', help='FTP Config')
         parser.add_argument('-u', '--username', help='WAAS Username')
         parser.add_argument('-p', '--password', help='WAAS Password')
-        parser.add_argument('-U', '--ftpUsername', help='FTP Username')
-        parser.add_argument('-P', '--ftpPassword', help='FTP Password')
 
         args = parser.parse_args()
 
@@ -408,7 +343,7 @@ def main(**kwargs):
     logger.info("FTP Config Imported")
     
     # Build WAE List
-    myWAEs = build_wae_list(waasList, ftpConfig, args.username, args.password, args.ftpUsername, args.ftpPassword)
+    myWAEs = build_wae_list(waasList, ftpConfig, args.username, args.password)
     
     # Set Device count
     deviceCount = len(myWAEs)
